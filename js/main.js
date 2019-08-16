0484daadac24370552bef29137c000c7907fcbb1
KrivulyaElena <krivulyaelena@gmail.com>
2019-08-16 17:51:37 +0300

    svg js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,52 @@
-$(function(){
-	$('.slider').slick({ 
-		slidesToShow:1,
-		slidesToScroll:1,		
-		dots:true,
-		fade:true,		
+$(function () {
+	$('.slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: true,
+		fade: true,
 		autoplay: true,
-        autoplaySpeed: 1500,
+		autoplaySpeed: 1500,
 		infinite: true,
 		// arrows:true,
 		// prevArrow:'<button class="slick-arrow slick-prev"><i class="fas fa-chevron-left"></i></button>',
 		// nextArrow:'<button class="slick-arrow slick-next"><i class="fas fa-chevron-right"></i></button>',
-		});
 	});
+});
 
-$(function(){
-	$('.toggle-menu').on('click',function(){
+$(function () {
+	$('.toggle-menu').on('click', function () {
 		$('.main-nav').toggleClass('open');
 	});
 });
 
 $(document).ready(function () {
 	new WOW().init();
+
+
+	$("a.menu__link").click(function () {
+		var elementClick = $(this).attr("href")
+		var destination = $(elementClick).offset().top;
+		jQuery("html:not(:animated),body:not(:animated)").animate({
+			scrollTop: destination
+		}, 800);
+		return false;
+
+	});
+
+	let footerLogoPosition = $('#logo-svg').offset().top;
+	var tlLogo = new TimelineMax();
+
+	$(window).on('scroll', function(e){
+		console.log($(this).scrollTop() + ' : ' + footerLogoPosition)
+		if ($(this).scrollTop() > (footerLogoPosition - $(this).height())){
+			tlLogo.to($('#logo-svg'), 3, {
+				'stroke-dashoffset': 0
+			})
+				.to($('#logo-svg'), .5, {
+					'fill': 'black'
+				})
+		}
+	})
 });
 
-var tlLogo = new TimelineMax();
-tlLogo.to($('#logo-svg'), 5, {
-        'stroke-dashoffset': 0
-	}, .01)
-	.to($('#logo-svg'),2, {
-		'fill': 'black'
-	})
+
